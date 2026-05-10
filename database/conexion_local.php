4c4e79c25df9d0e9c0d00f45f12f19fe2d04d5f4
LunaDiegoLeo <diegolhl678@gmail.com>
2026-05-10 02:51:52 -0600

    Remover archivo local del rastreo

--- database/conexion_local.php
+++
@@ -1,8 +0,0 @@
-<?php
-// credenciales de tu XAMPP / WAMP local
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', ''); // Usualmente vacío en local
-define('DB_NAME', 'entre_lineas');
-
-
